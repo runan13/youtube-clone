6284f91a1d40b0af09c415416d7a8998d4a1776e
runan13 <65391973+runan13@users.noreply.github.com>
2020-08-08 14:23:52 +0900

    Change Profile Page HTML

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -3,15 +3,15 @@ export const search = (req, res) => {
   const {
     query: { term: searchingBy },
   } = req;
-  res.render("Search", { pageTitle: "Search", searchingBy });
+  res.render("search", { pageTitle: "Search", searchingBy });
 };
 export const videos = (req, res) =>
-  res.render("Videos", { pageTitle: "Videos" });
+  res.render("videos", { pageTitle: "Videos" });
 export const upload = (req, res) =>
-  res.render("Upload", { pageTitle: "Upload" });
+  res.render("upload", { pageTitle: "Upload" });
 export const videoDetail = (req, res) =>
-  res.render("Video Detail", { pageTitle: "Video Detail" });
+  res.render("videoDetail", { pageTitle: "Video Detail" });
 export const editVideo = (req, res) =>
-  res.render("Edit Video", { pageTitle: "Edit Video" });
+  res.render("editVideo", { pageTitle: "Edit Video" });
 export const deleteVideo = (req, res) =>
-  res.render("Delete Video", { pageTitle: "Delete Video" });
+  res.render("deleteVideo", { pageTitle: "Delete Video" });
