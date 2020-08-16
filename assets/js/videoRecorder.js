852a33a08fce74d82e85f554fdf17e0a50c36cad
runan13 <65391973+runan13@users.noreply.github.com>
2020-08-16 18:44:44 +0900

    Error fix

---
+++ assets/js/videoRecorder.js
@@ -0,0 +1,27 @@
+const { doc } = require("prettier");
+const e = require("express");
+
+const recorderContainer = document.getElementById("jsRecordContainer");
+const recordBtn = document.getElementById("jsRecordBtn");
+const videoPreview = document.getElementById("jsVideoPreview");
+
+const statrRecording = async () => {
+  try {
+    const stream = await navigator.mediaDevices.getUserMedia({
+      audio: true,
+      video: true,
+    });
+    console.log(stream);
+  } catch (error) {
+    recordBtn.innerHTML = "😢 you can't record";
+    recordBtn.removeEventListener("click", statrRecording);
+  }
+};
+
+function init() {
+  recordBtn.addEventListener("click", statrRecording);
+}
+
+if (jsRecordContainer) {
+  init();
+}
