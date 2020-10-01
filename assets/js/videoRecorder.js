141f3070773745a2c3c65933c3f37fd664066427
runan13 <65391973+runan13@users.noreply.github.com>
2020-10-01 14:16:15 +0900

    Views

--- assets/js/videoRecorder.js
+++ assets/js/videoRecorder.js
@@ -1,6 +1,3 @@
-const { doc } = require("prettier");
-const e = require("express");
-
 const recorderContainer = document.getElementById("jsRecordContainer");
 const recordBtn = document.getElementById("jsRecordBtn");
 const videoPreview = document.getElementById("jsVideoPreview");
@@ -22,6 +19,6 @@ function init() {
   recordBtn.addEventListener("click", statrRecording);
 }
 
-if (jsRecordContainer) {
+if (recorderContainer) {
   init();
 }
--- routes.js
+++ routes.js
@@ -29,6 +29,10 @@ const GITHUB_CALLBACK = "/auth/github/callback";
 const FB = "/aute/facebook";
 const FB_CALLBACK = "/auth/facebook/callback";
 
+// API
+const API = "/api";
+const REGISTER_VIEW = "/:id/view";
+
 const routes = {
   home: HOME,
   join: JOIN,
@@ -73,6 +77,8 @@ const routes = {
   me: ME,
   facebook: FB,
   facebookCallback: FB_CALLBACK,
+  api: API,
+  registerView: REGISTER_VIEW,
 };
 
 export default routes;
